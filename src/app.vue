4411e727f211c244d7b442edc1d1555b6300aa6a
Dipu Sharma <sdipu231@gmail.com>
2024-08-11 16:33:39 +0530

    header add

--- src/app.vue
+++ src/app.vue
@@ -4,5 +4,13 @@
   </NuxtLayout>
 </template>
 
-<script>
+<script setup>
+import { useHead } from '#app'
+// console.log(Object.keys(obj));
+useHead({
+  title: 'D-shop',
+  meta: [
+    { name: 'description', content: 'Page-specific description' }
+  ]
+})
 </script>
--- src/components/AdminPage/Sidebar.vue
+++ src/components/AdminPage/Sidebar.vue
@@ -57,6 +57,18 @@
                 <span class="ml-4">Doss</span>
               </a>
             </li>
+            <li class="relative px-6 py-3">
+              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="#">
+                <span class="ml-4">Merchants</span>
+              </a>
+            </li>
+            <li class="relative px-6 py-3">
+              <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="#">
+                <span class="ml-4">Customer</span>
+              </a>
+            </li>
           </ul>
         </li>
         <li class="relative px-6 py-3">
