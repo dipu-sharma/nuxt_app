fdab8a24e1820e927c58121d671088c25bb343ff
DipuSharma <animesh.singh@unitecloud.in-at-825045453994>
2025-04-28 13:58:00 +0530

    commit

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,58 +1,10 @@
 <template>
-	<v-app>
-		<v-main>
-			<v-container>
-				<v-btn color="primary">About Page Hello, Vuetify!</v-btn>
-
-				<v-autocomplete
-					v-model="values"
-					:items="items.text"
-					density="comfortable"
-					label="Comfortable"
-				></v-autocomplete>
-				<h4>{{ child_payload.first_name }}</h4>
-				<h4>{{ child_payload.last_name }}</h4>
-
-				<div>
-					<Child :getPayload="getChildPayload" />
-				</div>
-			</v-container>
-		</v-main>
-	</v-app>
+	<div>
+		<h1>About</h1>
+	</div>
 </template>
 
-<script>
-import Child from '~/components/Child.vue'
-
-export default {
-	components: {
-		Child,
-	},
-	data() {
-		return {
-			items: [
-				{ text: 'California Ram is a king of Ayodhya...', value: 'california' },
-				{ text: 'Colorado', value: 'colorado' },
-				{ text: 'Florida', value: 'florida' },
-				{ text: 'Georgia', value: 'georgia' },
-				{ text: 'Texas', value: 'texas' },
-				{ text: 'Wyoming', value: 'wyoming' },
-			],
-			child_payload: {
-				first_name: '',
-				last_name: '',
-			},
-		}
-	},
-	methods: {
-		getChildPayload(payload) {
-			this.child_payload.first_name = payload.first_name
-			this.child_payload.last_name = payload.last_name
-		},
-	},
-	created() {},
-}
-</script>
+<script setup></script>
 
 <style scoped>
 .wrapped-item {
